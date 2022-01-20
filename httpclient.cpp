6b897168b1e85a902ffb7b68710f036c2b8d2120
Philipp de Zwart <philipp.de@bulme.at>
2022-01-20 17:36:50 +0000

    finished project

--- httpclient.cpp
+++ httpclient.cpp
@@ -1,12 +1,39 @@
 #include "httpclient.h"
+#include <QDebug>
 
 httpclient::httpclient(QWidget *parent) :
     QMainWindow(parent)
 {
     setupUi(this);
+    m_socket = new QTcpSocket();
+
+    connect(m_socket, &QTcpSocket::connected, this, &httpclient::tcp_connect);
+    connect(m_socket, &QTcpSocket::readyRead, this, &httpclient::tcp_receive);
 }
 
 void httpclient::on_g_absenden_clicked()
 {
+   g_output->clear();
+
+   QString serverAddress = g_address->text();
+   m_socket->connectToHost(serverAddress, m_port);
+   if(!m_socket->waitForConnected(5000))
+   {
+       g_output->setTextColor(QColor::fromRgb(255, 0, 0));
+       g_output->setText("Connection timed out!");
+       g_output->setTextColor(QColor::fromRgb(0, 0, 0));
+       m_socket->disconnectFromHost();
+   }
+}
+
+void httpclient::tcp_connect()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void httpclient::tcp_receive()
+{
+    g_output->setText(m_socket->readAll());
+    m_socket->disconnectFromHost();
 
 }
--- httpclient.h
+++ httpclient.h
@@ -2,6 +2,8 @@
 #define HTTPCLIENT_H
 
 #include "ui_httpclient.h"
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class httpclient : public QMainWindow, private Ui::httpclient
 {
@@ -11,6 +13,12 @@ public:
     explicit httpclient(QWidget *parent = nullptr);
 private slots:
     void on_g_absenden_clicked();
+    void tcp_connect();
+    void tcp_receive();
+private:
+    QTcpSocket* m_socket = nullptr;
+    const unsigned short m_port = 80;
+    QString m_hostname;
 };
 
 #endif // HTTPCLIENT_H
